003de956077a71c85ee4f236311e12490f0626ad
lupmariaa <maria.lup@student.upt.ro>
2023-12-07 13:37:39 +0200

    Lab

---
+++ src/main/java/Exemplu1/Dreptunghi.java
@@ -0,0 +1,31 @@
+package Exemplu1;
+
+public class Dreptunghi {
+    private int lungime;
+    private int latime;
+    public void aria()
+    {
+        System.out.println("Aria dreptunghi: "+(lungime*latime));
+    }
+
+    public Dreptunghi(int lungime, int latime) {
+        this.lungime = lungime;
+        this.latime = latime;
+    }
+
+    public int getLungime() {
+        return lungime;
+    }
+
+    public void setLungime(int lungime) {
+        this.lungime = lungime;
+    }
+
+    public int getLatime() {
+        return latime;
+    }
+
+    public void setLatime(int latime) {
+        this.latime = latime;
+    }
+}
